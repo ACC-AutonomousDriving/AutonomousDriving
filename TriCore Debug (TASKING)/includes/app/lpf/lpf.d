eead62047a8fb12b9763ae14ff96f6faebab54ee
Hong Ji Whan <h1234p@naver.com>
2024-05-13 18:23:44 +0900

    -

---
+++ TriCore Debug (TASKING)/includes/app/lpf/lpf.d
@@ -0,0 +1,8 @@
+includes/app/lpf/lpf.o :	../includes/app/lpf/lpf.c
+../includes/app/lpf/lpf.c :
+includes/app/lpf/lpf.o :	../includes/app/lpf/lpf.h
+../includes/app/lpf/lpf.h :
+includes/app/lpf/lpf.o :	c:/git_hub/auto-drive/Libraries/iLLD/TC27D/Tricore/Cpu/Std/Platform_Types.h
+c:/git_hub/auto-drive/Libraries/iLLD/TC27D/Tricore/Cpu/Std/Platform_Types.h :
+includes/app/lpf/lpf.o :	c:/git_hub/auto-drive/includes/commonMacros.h
+c:/git_hub/auto-drive/includes/commonMacros.h :
---
+++ TriCore Debug (TASKING)/includes/app/observer/observer.d
@@ -0,0 +1,20 @@
+includes/app/observer/observer.o :	../includes/app/observer/observer.c
+../includes/app/observer/observer.c :
+includes/app/observer/observer.o :	../includes/app/observer/observer.h
+../includes/app/observer/observer.h :
+includes/app/observer/observer.o :	c:/git_hub/auto-drive/Libraries/iLLD/TC27D/Tricore/Cpu/Std/Ifx_Types.h
+c:/git_hub/auto-drive/Libraries/iLLD/TC27D/Tricore/Cpu/Std/Ifx_Types.h :
+includes/app/observer/observer.o :	c:/git_hub/auto-drive/Libraries/Infra/Platform/Tricore/Compilers/Compilers.h
+c:/git_hub/auto-drive/Libraries/Infra/Platform/Tricore/Compilers/Compilers.h :
+includes/app/observer/observer.o :	c:/git_hub/auto-drive/Configurations/Ifx_Cfg.h
+c:/git_hub/auto-drive/Configurations/Ifx_Cfg.h :
+includes/app/observer/observer.o :	c:/git_hub/auto-drive/Libraries/Infra/Platform/Tricore/Compilers/CompilerTasking.h
+c:/git_hub/auto-drive/Libraries/Infra/Platform/Tricore/Compilers/CompilerTasking.h :
+includes/app/observer/observer.o :	c:/Infineon/AURIX-Studio-1.9.20/tools/Compilers/Tasking_1.1r8/ctc/include/stddef.h
+c:/Infineon/AURIX-Studio-1.9.20/tools/Compilers/Tasking_1.1r8/ctc/include/stddef.h :
+includes/app/observer/observer.o :	c:/git_hub/auto-drive/Libraries/iLLD/TC27D/Tricore/Cpu/Std/Platform_Types.h
+c:/git_hub/auto-drive/Libraries/iLLD/TC27D/Tricore/Cpu/Std/Platform_Types.h :
+includes/app/observer/observer.o :	c:/git_hub/auto-drive/Libraries/iLLD/TC27D/Tricore/Cpu/Std/Ifx_TypesTasking.h
+c:/git_hub/auto-drive/Libraries/iLLD/TC27D/Tricore/Cpu/Std/Ifx_TypesTasking.h :
+includes/app/observer/observer.o :	c:/git_hub/auto-drive/includes/commonMacros.h
+c:/git_hub/auto-drive/includes/commonMacros.h :
