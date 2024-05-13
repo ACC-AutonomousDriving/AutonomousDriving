e52375adb5769c8ab5a8da9797829247a2639619
ChaeYes <80445991+kche1106@users.noreply.github.com>
2024-05-13 11:16:57 +0900

    first commit

---
+++ auto-drive/TriCore Debug (TASKING)/sources.mk
@@ -0,0 +1,62 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+C++_SRCS := 
+CC_SRCS := 
+CPP_SRCS := 
+CXX_SRCS := 
+C_SRCS := 
+C_UPPER_SRCS := 
+OBJ_SRCS := 
+O_SRCS := 
+SRC_SRCS := 
+C++_DEPS := 
+CC_DEPS := 
+COMPILED_SRCS := 
+CPP_DEPS := 
+CXX_DEPS := 
+C_DEPS := 
+C_UPPER_DEPS := 
+EXECUTABLES := 
+OBJS := 
+SECONDARY_SIZE := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+Configurations/Debug \
+. \
+Libraries/Infra/Platform/Tricore/Compilers \
+Libraries/Service/CpuGeneric/StdIf \
+Libraries/Service/CpuGeneric/SysSe/Bsp \
+Libraries/Service/CpuGeneric/SysSe/Comm \
+Libraries/iLLD/TC27D/Tricore/Asclin/Asc \
+Libraries/iLLD/TC27D/Tricore/Asclin/Std \
+Libraries/iLLD/TC27D/Tricore/Cpu/CStart \
+Libraries/iLLD/TC27D/Tricore/Cpu/Irq \
+Libraries/iLLD/TC27D/Tricore/Cpu/Std \
+Libraries/iLLD/TC27D/Tricore/Cpu/Trap \
+Libraries/iLLD/TC27D/Tricore/Gtm/Std \
+Libraries/iLLD/TC27D/Tricore/Gtm/Tom/PwmHl \
+Libraries/iLLD/TC27D/Tricore/Gtm/Tom/Timer \
+Libraries/iLLD/TC27D/Tricore/Mtu/Std \
+Libraries/iLLD/TC27D/Tricore/Port/Std \
+Libraries/iLLD/TC27D/Tricore/Scu/Std \
+Libraries/iLLD/TC27D/Tricore/Src/Std \
+Libraries/iLLD/TC27D/Tricore/Stm/Std \
+Libraries/iLLD/TC27D/Tricore/_Impl \
+Libraries/iLLD/TC27D/Tricore/_Lib/DataHandling \
+Libraries/iLLD/TC27D/Tricore/_PinMap \
+includes/app/lpf \
+includes/app/observer \
+includes/app/pid \
+includes/app/taskScheduler \
+includes/hardware/encoder \
+includes/hardware/motor \
+includes/tc275/extIrq \
+includes/tc275/gpio \
+includes/tc275/pwmHl \
+includes/tc275/pwmTom \
+includes/tc275/stm \
+includes/tc275/uart \
+
